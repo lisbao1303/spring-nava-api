ff2eded7786dbda36971fcb6fe3ff5aa12be042d
lisbao1303 <gabriel.lisbao@gmail.com>
2025-01-07 19:08:25 -0300

    chore: initial commit

---
+++ src/main/java/com/elisbao/spring_nava_api/SpringNavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.elisbao.spring_nava_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNavaApiApplication.class, args);
+	}
+
+}
